83db53aaca5c9334f834a14e1d55fce7af3f2673
Daniel <daniilterawars@gmail.com>
2023-08-22 08:37:32 +0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
@@ -14,6 +15,15 @@ async function bootstrap() {
   });
 
   app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Universal API')
+    .setDescription('The e-commerce API for Universal app')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     console.log('[WEB]', `http://localhost:${port}`);
