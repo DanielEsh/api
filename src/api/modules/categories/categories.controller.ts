b875a6c34f63c6804c82d7b3520cecbb2e23c316
Daniel <daniilterawars@gmail.com>
2023-08-23 21:14:02 +0300

    feat: crud

--- src/api/modules/categories/categories.controller.ts
+++ src/api/modules/categories/categories.controller.ts
@@ -6,38 +6,74 @@ import {
   Patch,
   Param,
   Delete,
+  ParseArrayPipe,
+  DefaultValuePipe,
+  Query,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+
+const DEFAULT_VALUES = {
+  limit: 10,
+  page: 1,
+  sort_by: [],
+  order_by: [],
+};
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @ApiOperation({ summary: 'Create category' })
+  @ApiBody({ type: CreateCategoryDto })
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.createCategory(createCategoryDto);
   }
 
   @Get()
-  findAll() {
-    return this.categoriesService.findAll();
+  findAll(
+    @Query('page', new DefaultValuePipe(DEFAULT_VALUES.page), ParseIntPipe)
+    page,
+    @Query('limit', new DefaultValuePipe(DEFAULT_VALUES.limit), ParseIntPipe)
+    limit,
+    @Query(
+      'sort_by',
+      new DefaultValuePipe(DEFAULT_VALUES.sort_by),
+      ParseArrayPipe,
+    )
+    sort,
+    @Query(
+      'order_by',
+      new DefaultValuePipe(DEFAULT_VALUES.order_by),
+      ParseArrayPipe,
+    )
+    order,
+  ) {
+    return this.categoriesService.findAll({
+      sort,
+      order,
+      page,
+      limit,
+      route: ' ',
+    });
   }
 
   @Get(':slug')
   findOne(@Param('slug') slug: string) {
-    return this.categoriesService.findOne(slug);
+    return this.categoriesService.findOneBySlug(slug);
   }
 
-  @Patch(':id')
+  @Patch(':slug')
   update(
-    @Param('id') id: string,
+    @Param('slug') slug: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
-    return this.categoriesService.update(+id, updateCategoryDto);
+    return this.categoriesService.update(slug, updateCategoryDto);
   }
 
   @Delete(':id')
--- src/api/modules/categories/categories.service.ts
+++ src/api/modules/categories/categories.service.ts
@@ -1,20 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 // import { CrudService } from 'src/shared/crud/crud.service';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('categories')
+interface PaginationLinks {
+  previous: number | null;
+  next: number | null;
+}
+interface Pagination {
+  itemCountOnPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+  links: PaginationLinks;
+}
+
+export interface Meta {
+  totalItemsCount: number;
+  pagination: Pagination;
+}
+
+interface FindAllOptions {
+  sort: [string];
+  order: ['asc' | 'desc'];
+  limit: number;
+  page: number;
+  route: string;
+}
+
 @Injectable()
 export class CategoriesService {
   @InjectRepository(Category)
   private readonly repository: Repository<Category>;
 
-  @ApiOperation({ summary: 'Create category' })
-  @ApiBody({ type: CreateCategoryDto })
+  private readonly queryBuilder = null;
+
   async createCategory(createCategoryDto: CreateCategoryDto) {
     const categoryToCreate = new Category();
 
@@ -25,19 +48,94 @@ export class CategoriesService {
     return await this.repository.save(categoryToCreate);
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAll(options: FindAllOptions) {
+    const builder = await this.repository.createQueryBuilder('category');
+
+    return this.paginate(builder, options);
   }
 
-  findOne(slug: string) {
+  private async paginate(builder, options: FindAllOptions) {
+    const { limit = 10, page = 1 } = options;
+
+    console.log('options', options);
+
+    builder.limit(limit);
+    builder.offset((page - 1) * limit);
+
+    const totalItemsCount = await builder.getCount();
+    const getBuilder = await builder
+      .select('category.id')
+      .addSelect('category.slug')
+      .addSelect('category.name');
+
+    if (options.sort) {
+      options.sort.map((field, index) =>
+        getBuilder.orderBy(
+          `category.${field}`,
+          options.order.length ? options.order[index]?.toUpperCase() : 'ASC',
+        ),
+      );
+    }
+
+    const items = await getBuilder.getMany();
+
+    const totalPages =
+      totalItemsCount !== undefined
+        ? Math.ceil(totalItemsCount / limit)
+        : undefined;
+    const hasNextPage = page < totalPages;
+    const hasPreviousPage = page > 1;
+
+    const routes = {
+      previous: hasPreviousPage ? page - 1 : null,
+      next: hasNextPage ? page + 1 : null,
+    };
+
+    const meta: Meta = {
+      totalItemsCount,
+      pagination: {
+        itemCountOnPage: items.length,
+        itemsPerPage: limit,
+        totalPages,
+        currentPage: page,
+        links: {
+          previous: routes.previous,
+          next: routes.next,
+        },
+      },
+    };
+
+    return {
+      data: items,
+      meta,
+    };
+  }
+
+  findOneBySlug(slug: string) {
     return this.repository.findOne({ where: { slug } });
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  findOneById(id: number) {
+    return this.repository.findOne({ where: { id } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async update(slug: string, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOneBySlug(slug);
+
+    category.name = updateCategoryDto?.name ?? category.name;
+    category.description =
+      updateCategoryDto?.description ?? category.description;
+
+    return this.repository.save(category);
+  }
+
+  async remove(id: number) {
+    const deletedEntity = await this.findOneById(id);
+    if (!deletedEntity) {
+      throw new NotFoundException();
+    }
+
+    await this.repository.remove(deletedEntity);
+    return deletedEntity;
   }
 }
