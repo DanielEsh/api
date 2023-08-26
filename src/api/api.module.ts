588c0197fb031eca879b19a40137532bf3c3d4b1
Daniel <daniilterawars@gmail.com>
2023-08-26 11:47:16 +0300

    feat: brands

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { BrandsModule } from './modules/brands/brands.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, BrandsModule],
 })
 export class ApiModule {}
