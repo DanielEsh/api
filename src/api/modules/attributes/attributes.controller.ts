6914302a537eec90cbea78697b84f30e834fa99d
Daniel <daniilterawars@gmail.com>
2023-08-26 22:58:05 +0300

    feat(attribute): controller

--- src/api/modules/attributes/attributes.controller.ts
+++ src/api/modules/attributes/attributes.controller.ts
@@ -6,30 +6,107 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+  DefaultValuePipe,
+  Query,
+  ParseIntPipe,
+  ParseArrayPipe,
 } from '@nestjs/common';
 import { AttributesService } from './attributes.service';
 import { CreateAttributeDto } from './dto/create-attribute.dto';
 import { UpdateAttributeDto } from './dto/update-attribute.dto';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+} from '@nestjs/swagger';
+import { Attribute } from './entities/attribute.entity';
+import { Brand } from '../brands/entities/brand.entity';
+
+const DEFAULT_VALUES = {
+  limit: 10,
+  page: 1,
+  sort_by: [],
+  order_by: [],
+};
 
 @Controller('attributes')
 export class AttributesController {
   constructor(private readonly attributesService: AttributesService) {}
 
+  @ApiOperation({ summary: 'Create attribute' })
+  @ApiBody({ type: CreateAttributeDto })
+  @ApiCreatedResponse({ type: Attribute })
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() createAttributeDto: CreateAttributeDto) {
-    return this.attributesService.create(createAttributeDto);
+    try {
+      return this.attributesService.create(createAttributeDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.CONFLICT);
+    }
   }
 
+  @ApiOperation({ summary: 'Read attributes' })
+  @ApiQuery({ name: 'page', type: Number, example: 1, required: true })
+  @ApiQuery({ name: 'limit', type: Number, example: 10, required: true })
+  @ApiQuery({ name: 'sort_by', type: String, example: null, required: false })
+  @ApiQuery({ name: 'order_by', type: String, example: null, required: false })
   @Get()
-  findAll() {
-    return this.attributesService.findAll();
+  findAll(
+    @Query('page', new DefaultValuePipe(DEFAULT_VALUES.page), ParseIntPipe)
+    page,
+    @Query('limit', new DefaultValuePipe(DEFAULT_VALUES.limit), ParseIntPipe)
+    limit,
+    @Query(
+      'sort_by',
+      new DefaultValuePipe(DEFAULT_VALUES.sort_by),
+      ParseArrayPipe,
+    )
+    sortBy,
+    @Query(
+      'order_by',
+      new DefaultValuePipe(DEFAULT_VALUES.order_by),
+      ParseArrayPipe,
+    )
+    orderBy,
+  ) {
+    return this.attributesService.readAll({
+      sortBy,
+      orderBy,
+      page,
+      limit,
+    });
   }
 
+  @ApiOperation({ summary: 'Read attribute by id' })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 1,
+    required: true,
+  })
+  @ApiOkResponse({ type: Brand })
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.attributesService.findOne(+id);
+    return this.attributesService.readOneById(+id);
   }
 
+  @ApiOperation({ summary: 'Update attribute by id' })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    example: 1,
+    required: true,
+  })
+  @ApiBody({ type: UpdateAttributeDto })
+  @ApiOkResponse({ type: Brand })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +115,8 @@ export class AttributesController {
     return this.attributesService.update(+id, updateAttributeDto);
   }
 
+  @ApiOperation({ summary: 'Delete attribute by id' })
+  @ApiOkResponse({ type: Brand })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.attributesService.remove(+id);
