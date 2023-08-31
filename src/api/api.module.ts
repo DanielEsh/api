9f0fac9c0e556a982c67ec6420bf16bc16b91242
Daniel <daniilterawars@gmail.com>
2023-08-31 20:30:31 +0300

    feat(goods): init

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { BrandsModule } from './modules/brands/brands.module';
 import { AttributesModule } from './modules/attributes/attributes.module';
+import { GoodsModule } from './modules/goods/goods.module';
 
 @Module({
-  imports: [CategoriesModule, BrandsModule, AttributesModule],
+  imports: [CategoriesModule, BrandsModule, AttributesModule, GoodsModule],
 })
 export class ApiModule {}
