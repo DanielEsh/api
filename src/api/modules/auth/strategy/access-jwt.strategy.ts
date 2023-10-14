1d0892b3ddcfdf93aeac6311f4011b97ccd64ac6
Daniel <daniilterawars@gmail.com>
2023-10-14 16:46:26 +0300

    feat(auth): strategy

---
+++ src/api/modules/auth/strategy/access-jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthHelper } from '../helpers/auth.helpers';
+
+@Injectable()
+export class AccessJwtStrategy extends PassportStrategy(
+  Strategy,
+  'access-jwt',
+) {
+  @Inject(AuthHelper)
+  private readonly authHelper: AuthHelper;
+
+  constructor(@Inject(ConfigService) config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_ACCESS_KEY'),
+    });
+  }
+
+  async validate(payload) {
+    return {
+      id: payload.id,
+      name: payload.name,
+      email: payload.email,
+    };
+  }
+}
