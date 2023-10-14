d602f29b9f8453ebf4e66c2f67eb83586dda1bfc
Daniel <daniilterawars@gmail.com>
2023-10-14 17:19:53 +0300

    feat(auth): guard

---
+++ src/api/modules/auth/strategy/index.ts
@@ -0,0 +1,5 @@
+import { AccessJwtStrategy } from './access-jwt.strategy';
+import { RefreshJwtStrategy } from './refresh-jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+
+export { AccessJwtStrategy, RefreshJwtStrategy, LocalStrategy };
