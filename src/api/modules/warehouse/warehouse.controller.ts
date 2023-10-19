d6f09487f3f7afe29aa9eac5c2e72428dba5389a
Daniel <daniilterawars@gmail.com>
2023-10-19 20:58:18 +0300

    feat(warehouse): controller

---
+++ src/api/modules/warehouse/warehouse.controller.ts
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseArrayPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { WarehouseService } from './warehouse.service';
+import { CreateWarehouseDto } from './dto/create-warehouse.dto';
+import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
+
+const DEFAULT_VALUES = {
+  limit: 10,
+  page: 1,
+  sort_by: [],
+  order_by: [],
+};
+
+@Controller('warehouse')
+export class WarehouseController {
+  constructor(private readonly warehouseService: WarehouseService) {}
+  @Post()
+  create(@Body() createWarehouseDto: CreateWarehouseDto) {
+    try {
+      return this.warehouseService.create(createWarehouseDto);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.CONFLICT);
+    }
+  }
+
+  @Get()
+  findAll(
+    @Query('page', new DefaultValuePipe(DEFAULT_VALUES.page), ParseIntPipe)
+    page,
+    @Query('limit', new DefaultValuePipe(DEFAULT_VALUES.limit), ParseIntPipe)
+    limit,
+    @Query(
+      'sort_by',
+      new DefaultValuePipe(DEFAULT_VALUES.sort_by),
+      ParseArrayPipe,
+    )
+    sortBy,
+    @Query(
+      'order_by',
+      new DefaultValuePipe(DEFAULT_VALUES.order_by),
+      ParseArrayPipe,
+    )
+    orderBy,
+  ) {
+    return this.warehouseService.findAll({
+      sortBy,
+      orderBy,
+      page,
+      limit,
+    });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.warehouseService.findOneById(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateWarehouseDto: UpdateWarehouseDto,
+  ) {
+    return this.warehouseService.update(+id, updateWarehouseDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.warehouseService.remove(+id);
+  }
+}
